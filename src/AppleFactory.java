a30c1bbfa84f6dbf03e805c697445ba33ba7f343
meaningfullname <231233@astanait.edu.kz>
2024-10-03 19:11:53 +0500

    first commit

---
+++ src/AppleFactory.java
@@ -0,0 +1,11 @@
+class AppleFactory implements DeviceFactory {
+    @Override
+    public Smartphone createSmartphone() {
+        return new AppleSmartphone();
+    }
+
+    @Override
+    public Laptop createLaptop() {
+        return new AppleLaptop();
+    }
+}
---
+++ src/Smartphone.java
@@ -0,0 +1,4 @@
+public interface Smartphone {
+    void getDetails();
+
+}
